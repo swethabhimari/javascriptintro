88ce2fea6f10ec4f012728ea0c683aee64d5fbd2
Your Name <you@example.com>
2024-09-18 23:26:17 +0530

    new file is added

---
+++ js.js
@@ -0,0 +1,17 @@
+console.log("swetha")
+console.log("B")
+console.log(12345678)
+console.log(5);
+
+var age;//var declaration
+var age=40;//var assigning value
+
+var age=40;//initialization
+var job="frontend developer";
+var sal=50000;
+
+console.log( age );
+console.log( job );
+console.log( sal );
+console.log( movie );
+
